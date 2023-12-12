bb03a91d09108b6513b96d36e5d5b57f12d1635b
iskanderf <iskanderf.git@gmail.com>
2023-12-13 00:35:27 +0300

    edit type hints for python<3.10

--- utils.py
+++ utils.py
@@ -1,21 +1,22 @@
 import pandas as pd
+from typing import Union
 
 
 def find_duplicates(df: pd.DataFrame,
                     key_cols: list[str],
-                    return_bool: bool) -> pd.DataFrame | bool:
+                    return_bool: bool) -> Union[pd.DataFrame, bool]:
     dups = df[df.duplicated(subset=key_cols, keep='first')]
     if return_bool:
         return not dups.empty
     return dups
 
 
-def drop_duplicates(df: pd.DataFrame, key_cols: list[str]):
+def drop_duplicates(df: pd.DataFrame, key_cols: list[str]) -> pd.DataFrame:
     return df.drop_duplicates(subset=key_cols, keep='first')
 
 
 def find_missing_values(df: pd.DataFrame,
-                        return_bool: bool) -> pd.DataFrame | bool:
+                        return_bool: bool) -> Union[pd.DataFrame, bool]:
     missing_data = df.loc[df.isna().any(axis=1), :]
     if return_bool:
         return not missing_data.empty
